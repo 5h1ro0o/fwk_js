d2af58c964cb7a25a9a119aaf349baa0d1a5c29d
mateo <matteorobin@icloud.com>
2023-12-14 09:22:34 +0100

    commit

--- controllers/qcm.js
+++
@@ -1,7 +0,0 @@
-const qcms = require('../models/inmemory.js');
-const displayQcms = (req, res) => {
-    
-    res.render('qcms', {qcms: qcms});
-};
-
-module.exports = { displayQcms };
--- controllers/qcms.js
+++ controllers/qcms.js
@@ -1,9 +1,22 @@
-const qcms = require('../models/inmemory');
+const {qcms, addQcm} = require('../models/inmemory');
 
 const displayQcms = (req, res) => {
- 
+    for (let qcm of qcms) {
+        console.log(`${qcm.nbpoints}: ${typeof(qcm.nbpoints)}`);
+    }
     res.render('qcms', {qcms: qcms});
 };
+
+const displayQcmDetailed = (req, res) => {
+    const id = Number(req.params.qcmid);
+    console.log(id);
+    const qcm = qcms.find((element) => element.id === id);
+    res.render('qcm', {qcm});
+}
+
+const displayQcmJson = (req, res) => {
+    res.json({qcms});
+}
 
 const displayFormQcm = (req, res) => {
     res.render('newqcm');
@@ -11,8 +24,12 @@ const displayFormQcm = (req, res) => {
 
 const createNewForm = (req, res) => {
     console.log(req.body);
-    qcms.push(req.body.title);
+    addQcm({
+        name: req.body.name,
+        subject: req.body.subject,
+        nbpoints: req.body.nbpoints
+    });
     res.redirect('/qcms');
 };
 
-module.exports = {displayQcms, displayFormQcm, createNewForm};
+module.exports = {displayQcms, displayFormQcm, createNewForm, displayQcmJson, displayQcmDetailed};
